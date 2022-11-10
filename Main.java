7a1b17526bb5108cc601853c6e367de60a77df05
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-10 23:25:01 +0530

    ADDED: Code to print element present on an even position

--- Main.java
+++ Main.java
@@ -1,16 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        ElementOfArray ele = new ElementOfArray();
-        ele.element();
+        Main mObj = new Main();
+        mObj.element();
+        mObj.even();
 
     }
-    public static class ElementOfArray {
-        public void element() {
-            int[] arr = {1, 3, 5, 6, 3, 54, 63, 21, 65};
-            for (int j : arr) {
-                System.out.print(j + " ");
+
+    //    Java Program to print the elements of an array
+    public void element() {
+        int[] arr = {1, 3, 5, 6, 3, 54, 63, 21, 65};
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+    }
+
+    //    Java Program to print the elements of an array present on an even position
+    public void even() {
+        int arr[] = {101, 4, 1, 23, 21, 11, 34, 76, 43, 22, 43, 76, 95};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.println(i + " is even position of index");
             }
         }
     }
+
 
 }
