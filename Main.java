c89d46fa40fb42cd67c0ce8712ccb9485698eb64
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-10 23:15:52 +0530

    ADDED: Code to print element of array

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        
+        ElementOfArray ele = new ElementOfArray();
+        ele.element();
+
     }
+    public static class ElementOfArray {
+        public void element() {
+            int[] arr = {1, 3, 5, 6, 3, 54, 63, 21, 65};
+            for (int j : arr) {
+                System.out.print(j + " ");
+            }
+        }
+    }
+
 }
